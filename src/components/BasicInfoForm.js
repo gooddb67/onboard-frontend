7202d59bfbaf8eff21bbfec277f62f427bc56054
cpan001 <github email address>
2017-11-09 13:33:38 -0500

    added basic info form

--- src/components/BasicInfoForm.js
+++ src/components/BasicInfoForm.js
@@ -7,22 +7,61 @@ export default class BasicInfoForm extends React.Component {
         <h1>Sign Up!</h1>
         <form>
           <label htmlFor="email">Email:</label>
-          <input id="email" type="text" />
+          <input
+            id="email"
+            type="text"
+            value={this.props.formInfo.email}
+            onChange={e => {
+              this.props.handleChange(e.target.id, e.target.value);
+            }}
+          />
           <br />
           <label htmlFor="name">Full Name:</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            value={this.props.formInfo.name}
+            onChange={e => {
+              this.props.handleChange(e.target.id, e.target.value);
+            }}
+          />
           <br />
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" />
+          <input
+            type="password"
+            id="password"
+            value={this.props.formInfo.password}
+            onChange={e => {
+              this.props.handleChange(e.target.id, e.target.value);
+            }}
+          />
           <br />
           <label htmlFor="avatar">Avatar:</label>
-          <input type="text" id="avatar" />
+          <input
+            type="text"
+            id="avatar"
+            value={this.props.formInfo.avatar}
+            onChange={e => {
+              this.props.handleChange(e.target.id, e.target.value);
+            }}
+          />
           <br />
           <label htmlFor="admin">Admin:</label>
-          <input type="checkbox" id="admin" />
+          <input
+            type="checkbox"
+            id="admin"
+            value={this.props.formInfo.admin}
+            onChange={e => {
+              this.props.handleChange(e.target.id, e.target.checked);
+            }}
+          />
           <br />
           <br />
-          <input type="button" value="Next" />
+          <input
+            type="button"
+            value="Next"
+            onClick={this.props.onFirstFormSubmit}
+          />
         </form>
       </div>
     );
--- src/components/SignupContainer.js
+++ src/components/SignupContainer.js
@@ -2,10 +2,53 @@ import React from "react";
 import BasicInfoForm from "./BasicInfoForm";
 
 export default class SignupContainer extends React.Component {
+  state = {
+    email: "",
+    name: "",
+    password: "",
+    avatar: "",
+    admin: false
+  };
+
+  handleFirstFormSubmit = event => {
+    console.log(event);
+  };
+
+  handleChange = (id, value) => {
+    console.log(id, value);
+    switch (id) {
+      case "email":
+        this.setState({ email: value });
+        break;
+      case "name":
+        this.setState({ name: value });
+        break;
+      case "password":
+        this.setState({ password: value });
+        break;
+      case "avatar":
+        this.setState({ avatar: value });
+        break;
+      case "admin":
+        this.setState({ admin: value });
+        break;
+    }
+  };
+
   render() {
     return (
       <div>
-        <BasicInfoForm />
+        <BasicInfoForm
+          onFirstFormSubmit={this.handleFirstFormSubmit}
+          handleChange={this.handleChange}
+          formInfo={{
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password,
+            avatar: this.state.avatar,
+            admin: this.state.admin
+          }}
+        />
       </div>
     );
   }
